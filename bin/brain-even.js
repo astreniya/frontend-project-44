b5b095306b8c57dba37dbba54c097bae43a95cf1
astreniya <astreniya@kodeks.ru>
2026-07-01 14:06:52 +0300

    upd brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,41 @@
 #!/usr/bin/env node
-let correctAnswer = 'no'
-let name = 'Bill'
-let answer = 'yes'
+
+import readlineSync from 'readline-sync'
 
 console.log('Welcome to the Brain Games!')
-console.log(`May I have your name? ${name}`)
+let name = readlineSync.question('May I have your name? ')
 console.log(`Hello, ${name}!`)
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
-console.log('15')
-console.log(`Your answer:  ${answer}`)
-if (answer === correctAnswer) {
-    console.log('Correct!')
-} else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n` +
-        `Let's try again, ${name}!`)
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+for (let i = 0; i < 3; i++) {
+  let number = getRandomIntInclusive(1, 100)
+  console.log(`Question: ${number}`)
+  let answer = readlineSync.question('Your answer: ')
+
+  if (number % 2 === 0) {
+    let correct = 'yes'
+    if (answer === correct) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\n` + `Let's try again, ${name}!`)
+      break
+    }
+  }
+  else {
+    let correct = 'no'
+    if (answer === correct) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\n` + `Let's try again, ${name}!`)
+      break
+    }
+  }
 }
