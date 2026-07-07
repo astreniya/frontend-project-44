54c6ddd9090eed7ec23be7faf44dd48fc6ab6f28
Olga <30654708+astreniya@users.noreply.github.com>
2026-07-07 13:36:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import readlineSync from 'readline-sync'
 
 export default (gameTitle, example, correctAnswer) => {
   console.log('Welcome to the Brain Games!')
-  let name = readlineSync.question('May I have your name? ')
+  const name = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${name}!`)
   console.log(gameTitle)
 
   for (let i = 0; i < 3; i++) {
     console.log(`Question: ${example[i]}`)
 
-    let answer = readlineSync.question('Your answer: ')
+    const answer = readlineSync.question('Your answer: ')
 
     if (answer.toString() === correctAnswer[i]) {
       console.log('Correct!')
